8bd9852a28ca96bfa1e08bf26ff019dff15903e4
DragonMo <975425198@qq.com>
2025-05-09 21:27:59 +0800

    init

--- mianbaoren/src/app/page.tsx
+++ mianbaoren/src/app/page.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
-
+import bg from "@/img/mianbaoren.png"
 export default function Home() {
     return (
         <div
-            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-2   gap-16   font-[family-name:var(--font-geist-sans)]">
+
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                <div>一车面包人</div>
-                <a className="link" href={"https://soulstation.club"}>免费的MBTI测试</a>
+                <div>mianbao.ren 一车面包人</div>
+                <Image width={200} src={bg} alt={"一车面包人"}></Image>
+
+                <a className="link" href={"https://soulstation.club"}>SoulStation 荣格八维测试</a>
+                <a className="link" href={"https://www.jungus.cn/"}>荣格斯 荣格八维测试</a>
+                <a className="link" href={"https://www.apesk.com/jungtest/"}>才储 荣格八维测试</a>
+                <a className="link" href={"https://totypes.com/xinggeceshi.php"}>totypes 荣格八维测试</a>
+                <a className="link" href={"https://www.16personalities.com/"}>16personalities(16P小人) NERIS测试</a>
             </main>
 
 
