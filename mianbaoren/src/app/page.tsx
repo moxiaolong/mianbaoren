f76d701cfd63a30428bb6586599f4abea8816a0e
dragon <975425198@qq.com>
2025-12-02 22:04:56 +0800

    内容更新

--- mianbaoren/src/app/page.tsx
+++ mianbaoren/src/app/page.tsx
@@ -36,36 +36,43 @@ export default function Home() {
             title: "SoulStation 荣格八维测试",
             url: "https://soulstation.club",
             rating: 5,
-            desc: "排序法的免费荣格八维测试，型式新颖。虽然只有70道题，但其实是为了降低理解成本进行了场景融合，280个选项还是很丰富的。内容说人话，不故弄玄虚，报告详细免费。体验优秀，可以登录保存结果，也可以免登录查看结果，推荐。"
+            desc: "排序法的免费荣格八维测试，型式新颖，题目也很新。虽然只有70道题，但有280个选项。题目通俗易懂，从非理性场景到理性场景循序渐进，可以避免过早的进入理性状态远离对潜意识的考察，推荐。"
+        },
+        {
+            title: "MoonType 荣格八维测试",
+            url: "https://moontype.fun/",
+            rating: 4,
+            desc: "MoonType的作者对荣格功能的理解很好，对功能的理解比Totypes更贴近原著。免费，200题，五度选择，一些难以考察的功能相关的问题会略显重复。"
         },
         {
             title: "Pdb类型数据库(非测试)",
             url: "https://www.personality-database.com/",
-            rating: 5,
+            rating: 4,
             desc: "Personality Database人格类型数据库，包含大量历史人物、现世人物、虚拟角色，甚至是概念的MBTI和九型类型数据，数据由用户投票得出，投票需说明缘由，可信度较高。"
         },
+
         {
             title: "Totypes 荣格八维测试",
             url: "https://totypes.com/xinggeceshi.php",
-            rating: 5,
-            desc: "专业度较高的荣格八维测试。题目略显晦涩，是两功能迫选，可能会碰到两者皆否但无法表达的情况。测试和初步报告免费，深度报告收费。需要登录才能测试。",
+            rating: 3,
+            desc: "比荣格斯更接八维理论，但还是和原著理论有较大差距，给我的感觉是内外倾两端迫选+二分法迫选。测试和初步报告免费，深度报告收费。需要登录才能测试。",
         },
         {
             title: "荣格斯 荣格心里测试",
             url: "https://www.jungus.cn/",
-            rating: 3,
+            rating: 2,
             desc: "荣格八维圈传播最广的测试，但其实和荣格八维理论有较大出入，题目较老，疑似暂停维护，比起类型论更像是特质论。",
         },
         {
             title: "才储 荣格认知功能测试",
             url: "https://www.apesk.com/jungtest/",
-            rating: 2,
+            rating: 1,
             desc: "才储主要面向企业，对免费测试不太上心，出的较早，但认可度一般。",
         },
         {
             title: "16personalities NERIS测试",
             url: "https://www.16personalities.com/",
-            rating: 2,
+            rating: 1,
             desc: "传播非常广泛，小人形象深入人心。经常被误认为是MBTI官网，但题目和解析都是特质论，披着MBTI字母的外衣，但却是大五人格。",
         },
     ];
@@ -116,7 +123,7 @@ export default function Home() {
                             <div className="flex items-center w-full justify-between">
                                 <div className="flex items-center">
 
-                                    {link.rating === 5 && <span
+                                    {link.rating >=4 && <span
 
                                         className={`text-lg pr-1 ${
                                             'text-amber-400' // 金色星星
